894cd75743ab65cbf19d5660b7e4c5cdd3716d02
Lean <basedlainpilled@gmail.com>
2022-12-16 01:31:49 -0500

    move contracts to flamingo

--- src/Avatar.sol
+++ src/Avatar.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.17;
 
-import "./URIStorage.sol";
+import "lib/flamingo/src/URIStorage.sol";
 
 contract Avatar is URIStorage {
     constructor() URIStorage("Avatar", "AVATAR") {}
